080fdf8e795836f35aa68daa86ea9be6617ee50b
root <root@SQ1-DT-015.ad.sq1.security>
2025-04-30 12:27:19 +0000

    kkkkkk

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -35,7 +35,7 @@ class TransactionController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        // dd($request->all());
         $request->validate([
             'title' => 'required|string|max:255',
             'amount' => 'required|numeric',
