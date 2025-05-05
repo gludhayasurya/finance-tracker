d7b19864ee713c48da1d046609d6b0185594aa40
root <root@SQ1-DT-015.ad.sq1.security>
2025-05-05 12:35:23 +0000

    sweetalert

--- resources/views/partials/datatables.blade.php
+++ resources/views/partials/datatables.blade.php
@@ -20,13 +20,31 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script>
         $(document).ready(function() {
+            alert("1")
+            showToast('success', 'hihi');
+
             $('#mydataTable').DataTable({
                 dom: 'Bfrtip',
                 buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
                 pageLength: 10
             });
         });
+
+        function showToast(type = 'success', message = 'Operation done!') {
+            Swal.fire({
+                toast: true,
+                position: 'top-end',
+                icon: type,
+                title: message,
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+            });
+        }
+
     </script>
 @stop
